07446d1ca564c90f4fe2c99e18a806aab8971cd1
Bogdan Dinchev <bogdan.dinchev@interop.io>
2024-02-14 16:52:34 +0200

    subscribe for ISIN changes through WS api

--- src/AdaptableAgGrid/SimilarBondsBlotter.tsx
+++ src/AdaptableAgGrid/SimilarBondsBlotter.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { Root } from 'react-dom/client';
 import { GridOptions } from '@ag-grid-community/core';
 import { AgGridReact } from '@ag-grid-community/react';
 import AdaptableReact, {
   AdaptableApi,
   AdaptableOptions,
+  ColumnFilter,
 } from '@adaptabletools/adaptable-react-aggrid';
 import { columnDefs, defaultColDef } from './columnDefs';
 import { rowData, SimilarBond } from './rowData';
 import { agGridModules } from './agGridModules';
 import { renderReactRoot } from '../react-18-utils';
+import { useIOConnect } from '@interopio/react-hooks';
 
 const renderWeakMap: WeakMap<HTMLElement, Root> = new WeakMap();
 
@@ -67,6 +69,54 @@ export const SimilarBondsBlotter = () => {
   );
 
   const adaptableApiRef = React.useRef<AdaptableApi>();
+  const ISIN_CONTEXT = 'ISIN_CONTEXT';
+
+  useEffect(() => {
+    async function setMyWorkspaceId() {
+      const inWsp = await (window as any).io.workspaces?.inWorkspace();
+      if (!inWsp) {
+        return;
+      }
+
+      const myWorkspace = await (window as any).io.workspaces?.getMyWorkspace();
+      await (window as any).io.windows.my().updateContext({
+        workspaceId: myWorkspace?.id,
+      });
+    }
+
+    setMyWorkspaceId();
+  }, []);
+
+  useIOConnect(async (io) => {
+    const workspaceId = (await io.windows.my().getContext()).workspaceId;
+
+    const workspace =
+      (await io.workspaces?.getAllWorkspaces())?.find(
+        ({ id }) => id === workspaceId,
+      ) || (await io.workspaces?.getMyWorkspace());
+    if (!workspace) return;
+
+    return workspace.onContextUpdated((context: any) => {
+      const adaptableApi = adaptableApiRef.current;
+
+      if (adaptableApi) {
+        if (ISIN_CONTEXT in context) {
+          const isinValue = context[ISIN_CONTEXT].id?.ISIN;
+          const isinFilter: ColumnFilter = {
+            ColumnId: 'ISIN',
+            Predicate: {
+              PredicateId: 'Is',
+              Inputs: [isinValue],
+            },
+          };
+          adaptableApi.filterApi.setColumnFilters([isinFilter]);
+        } else {
+          // TODO - investigate and remove
+          adaptableApi.filterApi.clearColumnFilters();
+        }
+      }
+    });
+  });
 
   return (
     <div
