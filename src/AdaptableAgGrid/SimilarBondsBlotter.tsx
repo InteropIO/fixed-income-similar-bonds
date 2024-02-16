94b6854a32c1ff5ec8a700294c6cee6db489ec6e
Veneta Grigorova <veneta.grigorova@tick42.com>
2024-02-16 14:53:07 +0200

    clearing table filters on startup to fix rerendering bug

--- src/AdaptableAgGrid/SimilarBondsBlotter.tsx
+++ src/AdaptableAgGrid/SimilarBondsBlotter.tsx
@@ -40,6 +40,9 @@ export const SimilarBondsBlotter = () => {
       autogeneratePrimaryKey: true,
       userName: 'Test User',
       adaptableId: 'AdaptableFinsembleSimilarBonds',
+      filterOptions: {
+        clearFiltersOnStartUp: true,
+      },
       predefinedConfig: {
         Dashboard: {
           DashboardTitle: 'Similar Bonds',
